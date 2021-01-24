c679ec829bec899d3e5f5200f9e6e95513e0df92
santugowda <meetsanthoshbl@gmail.com>
2021-01-24 06:59:52 -0800

    Added users list source and viewmodel

---
+++ app/src/main/java/com/example/githubrepo/presentation/datasource/UsersListDataSourceFactory.kt
@@ -0,0 +1,18 @@
+package com.example.githubrepo.presentation.datasource
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.example.githubrepo.data.api.GithubApiClient
+import com.example.githubrepo.data.model.GithubUserModel
+
+class UsersListDataSourceFactory(private val githubApiClient: GithubApiClient): DataSource.Factory<Int, GithubUserModel>() {
+
+    val liveData: MutableLiveData<UsersListDataSource> = MutableLiveData()
+
+    override fun create(): DataSource<Int, GithubUserModel> {
+        val usersListDataSource = UsersListDataSource(githubApiClient)
+        liveData.postValue(usersListDataSource)
+        return usersListDataSource
+    }
+
+}
